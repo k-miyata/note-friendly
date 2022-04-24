d9ae1843412cde621068a6bddcf6620b9f6816a6
Kazuhiro Miyata <18201546+k-miyata@users.noreply.github.com>
2022-04-24 18:35:24 +0900

    Change to use custom properties

--- src/main.js
+++ src/main.js
@@ -1,17 +1,30 @@
-function changeBackground() {
-  document
-    .querySelector('main.p-article:not([hidden])')
-    .classList.add('notemu-chromeext', 'enhanced-background');
+/**
+ * Returns whether the user is on one of the pages on Note.
+ */
+function isNoteDomain() {
+  return document.location.hostname === "note.com";
 }
 
-function isArticlePage() {
-  const path = location.pathname;
-  // "https://note.mu/<Username>/n/<Note ID>" or "https://<Domain>/n/<Note ID>"
-  return /^\/[\w\-]+\/n\/n[a-z0-9]{12}$/.test(path) || /^\/n\/n[a-z0-9]{12}$/.test(path);
+/**
+ * Returns whether the user is on one of the article pages on Note connected to
+ * a custom domain. For these pages, applies the style only to the article pages
+ * that can be exactly determined to be on Note.
+ */
+function isArticlePageOnCustomDomain() {
+  return /^\/n\/n[a-z0-9]{12}$/.test(document.location.pathname);
 }
 
-document.body.addEventListener('transitionend', () => {
-  if (isArticlePage()) changeBackground();
-});
+/**
+ * Adds the class to `<html>` to apply the styles.
+ */
+function applyStyles() {
+  document.documentElement.classList.add("chrome-extension-note-friendly");
+}
 
-if (isArticlePage()) changeBackground();
+// This content script is injected after the DOM is complete, so you can access
+// the DOM without listening to the `DOMContentLoaded` event. See the
+// documentation for details on when content scripts are injected:
+// https://developer.chrome.com/docs/extensions/mv3/content_scripts/#run_time.
+if (isNoteDomain() || isArticlePageOnCustomDomain()) {
+  applyStyles();
+}
